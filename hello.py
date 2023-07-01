be4ed3f6d302f653a29c50f95448f54f1a3ecafe
Nayi-Schaeffer <nayaretschaeffer08@gmail.com>
2023-07-01 00:17:09 -0400

    python

---
+++ hello.py
@@ -0,0 +1,6 @@
+#Python es un lenguaje formal, es decir, que tiene reglas muy estrictas sobre la estructura del código. en cambio los lenguajes naturales, como el castellano, tienen reglas que dan lugar a interpretación diferente para cada persona ej: ironía, sarcasmo, poesía, etc. Python no. Es estricto y no tine lugar para diferentes interpretaciones.
+#EOF: End of file
+#EOL End of line
+print(1 + 4)
+print("Hola"+'python')
+print("hola"*3)
---
+++ types.py
@@ -0,0 +1,14 @@
+#Python tare varios tipos de valores o de datos. Estos valores que python trae se les conoce como tipo primitivos, para diferenciarlos de los tipos que puede crear el mismo programador.
+from re import T
+
+
+print(type(3))#int
+print(type(3.5))#float
+print(type("hello"))#str
+print(type('word'))#str
+print(type(True))#Bool
+print(type(False))#Bool
+print(type("42"))#Str
+
+
+
