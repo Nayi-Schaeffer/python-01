c4111a710f01f2190b7e208a22205b0c25f6197b
Nayi-Schaeffer <nayaretschaeffer08@gmail.com>
2023-07-01 12:12:13 -0400

    python_repaso

---
+++ 05-conditionals.py
@@ -0,0 +1,52 @@
+#Asignación y reacisignación
+# La asignación es dar un nombre a un valor, es decir asignar una variable a un valor.
+#Ej:
+
+fruit = "Chocolate"
+veggie = "Papas fritas"
+tables = "5"
+
+#Mientras que la reasignación es tomar una variable ya existente y darle un valor completamente diferente.
+#Ej:
+
+name = "Naya"
+name = "Nayaret"
+
+#Ejecución condicional.
+#Todos los programas hacen evaluaciones condicionales segun el valor de sus variables y expresiones. Por lo tanto no todas las lineas de codigo se ejecutan cuando corre un programa. 
+#Para esto esta el concepto de expresión Booleana
+
+#Ej:
+print(5 < 4) #False
+print(5 > 4) #True
+print(4 != 4) #False
+print(4 == 4) #True
+
+#Cuando queremos comparar menor o igual el opreador es <= 
+print(4 <= 6)
+
+#El mayo o igual es >=
+print(5 >= 6)
+
+#No existe el =< ni el =>
+
+
+#Podemos tener expresiones booleanas mas complejas utilizando los operadores and, or y not.
+
+print("_____________Expresiones booleanas compuestas______________")
+print(4 < 5 or 6 > 8)#True or False = Todo esto se simplifica en True 
+print(4 < 5 and 6 >8)#True anda False = 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 06-conditionals2.py
@@ -0,0 +1,8 @@
+#Las expresiones booleanas nos permiten hacer flujos de ejecución condicional utilizando estrucuturas de control.
+age = int(input("Cual es tu edad"))
+
+if age >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Entonces eres menor de edad")
+    
